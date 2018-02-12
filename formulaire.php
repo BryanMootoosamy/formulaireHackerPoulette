f820c751502e235133f2108732ce843f9aa52e02
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-02-12 09:49:36 +0100

    1

---
+++ formulaire.php
@@ -0,0 +1,4 @@
+<section class="formulaire">
+  <h1>Formulaire de contact</h1>
+  <img class="logo" src="content/hackers-poulette-logo.png" alt="Logo">
+</section>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="assets/style.css">
+    <meta charset="utf-8">
+    <title>Hackers Poulette - Formulaire de contact</title>
+  </head>
+  <body>
+    <?php require "formulaire.php" ?>
+  </body>
+</html>
