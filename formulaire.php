7ee7e790d13ed0a02669a4a3193008c5d1810036
MignotMorgan <MignotMorgan@gmail.com>
2018-02-12 11:30:24 +0100

    Morgan: création de formulaire du message

--- formulaire.php
+++ formulaire.php
@@ -1,4 +1,12 @@
 <section class="formulaire">
   <h1>Formulaire de contact</h1>
   <img class="logo" src="content/hackers-poulette-logo.png" alt="Logo">
+  <section class="section--form-msg">
+      <form class="form--msg" action="index.php" method="post">
+        <input class="form--msg-sujet" type="text" name="sujet" value="" placeholder="Sujet du message.">
+        <textarea class="form--msg-area" name="msg-area" rows="8"></textarea>
+        <input class="form--msg-btn" type="submit" name="" value="Envoyer">
+      </form>
+  </section>
+
 </section>
